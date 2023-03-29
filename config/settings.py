59ff4da919bb5141eb56802e2f975bb94e6611f3
Sony03 <106645312+Sony03@users.noreply.github.com>
2023-03-29 16:01:46 +0900

    ALLOWED_HOSTS 변경

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '7s_&k6woi6mwqhd_kwbvww57#87q%%@&h8n09rvcfc^)-l#jj1'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.39.137.154']
 
 
 # Application definition
